27176537c8174d4d97772f54c7e70f3db039c404
aastha saraf <aasthasaraf@aasthas-MacBook-Pro.local>
2020-10-02 23:21:14 -0400

    Change 1

---
+++ build/precache-manifest.b5924416a281d4b6ed9afede0f4437f4.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bfbe999fe6460606162d8647c9815e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f94f81cc9e03d71fcfe0",
+    "url": "/static/css/3.879e9400.chunk.css"
+  },
+  {
+    "revision": "e6d4155de2656141e050",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "178cf1802f00ad1f8942",
+    "url": "/static/css/main.4d5be1ce.chunk.css"
+  },
+  {
+    "revision": "2a7f2a2d6827b5dabe2b",
+    "url": "/static/js/2.8395e5ad.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/static/js/2.8395e5ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f94f81cc9e03d71fcfe0",
+    "url": "/static/js/3.1a2ec98d.chunk.js"
+  },
+  {
+    "revision": "e6d4155de2656141e050",
+    "url": "/static/js/4.4652ee90.chunk.js"
+  },
+  {
+    "revision": "178cf1802f00ad1f8942",
+    "url": "/static/js/main.827c5794.chunk.js"
+  },
+  {
+    "revision": "cd5f6fbec6ab6901fb88",
+    "url": "/static/js/runtime-main.a3a20080.js"
+  },
+  {
+    "revision": "a156a76d959dd7a261272a8f8e71b3bc",
+    "url": "/static/media/Aerotis.a156a76d.otf"
+  },
+  {
+    "revision": "79f83e1abb1b995065f381d9b8a845dc",
+    "url": "/static/media/BSNL.79f83e1a.png"
+  },
+  {
+    "revision": "445a65250815657b7c0d03f52590c0b4",
+    "url": "/static/media/Can you Escape.445a6525.png"
+  },
+  {
+    "revision": "bea31ec273c09434c708af8e78b4c8ff",
+    "url": "/static/media/Dont judge.bea31ec2.png"
+  },
+  {
+    "revision": "1c2629aa1045f162e0893f77702fa803",
+    "url": "/static/media/Dream cubicle.1c2629aa.png"
+  },
+  {
+    "revision": "642437ceacb6fd6e8ecd2330deda7c1d",
+    "url": "/static/media/Facebook.642437ce.png"
+  },
+  {
+    "revision": "324a513cb9c683dc30777bdbccf59995",
+    "url": "/static/media/JosefinSans-VariableFont_wght.324a513c.ttf"
+  },
+  {
+    "revision": "477bb0b23b42fe97b052a7de72188c06",
+    "url": "/static/media/Mail_icon.477bb0b2.png"
+  },
+  {
+    "revision": "4c1baf2f7c89a1a497c8e84ce56811dc",
+    "url": "/static/media/Traffic Sign.4c1baf2f.png"
+  },
+  {
+    "revision": "2ce37aedd8c33b654ff34641dcaec83d",
+    "url": "/static/media/UIC.2ce37aed.png"
+  },
+  {
+    "revision": "0e3b11e6ebfa2543650a2075e7982f45",
+    "url": "/static/media/VR.0e3b11e6.png"
+  },
+  {
+    "revision": "942b7ed5c53aa2593738d17e9897fbbe",
+    "url": "/static/media/gun shot.942b7ed5.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "647fdeca5a8bda6622c412d1c94b784e",
+    "url": "/static/media/workingwoman.647fdeca.png"
+  }
+]);
---
+++ src/portfolio.js
@@ -0,0 +1,318 @@
+﻿
+/* Change this file to get your personal Portfolio */
+
+// Your Summary And Greeting Section
+
+import emoji from "react-easy-emoji";
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  username: "Aastha Saraf",
+  title: "How you doin? I'm Aastha",
+  subTitle: emoji("A creative AR/VR and ML researcher having an experience of building appealing Web applications with JavaScript, Reactjs and some other frameworks."),
+  resumeLink: "https://drive.google.com/file/d/1nvmfMhuSbkr0BKXDhIydtHEw9liNH02u/view?usp=sharing"
+};
+
+// Your Social Media Link
+
+const socialMediaLinks = {
+
+  github: "https://github.com/asaraf1995",
+  linkedin: "https://www.linkedin.com/in/aasthasaraf/",
+  gmail: "aasthasaraf13@gmail.com",
+  facebook: "https://www.facebook.com/aastha.saraf.77"
+  // Instagram and Twitter are also supported in the links!
+};
+
+// Your Skills Section
+
+const skillsSection = {
+  title: "What I want to do?",
+  subTitle: "To explore different domains of Computer Science",
+  skills: [
+    emoji("📌 Solve a real-life problem using Computer Science to improve society"),
+    emoji("📌 Combine Arts and Music with Deep Learning"),
+    emoji("📌 Become an inspiration for young girls to pursue Computer Science")
+  ],
+
+/* Make Sure You include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "bootstrap",
+      fontAwesomeClassname: "fab fa-bootstrap"
+    },
+    {
+      skillName: "unity",
+      fontAwesomeClassname: "fab fa-unity"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "fab fa-aws"
+    },
+    
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "java",
+      fontAwesomeClassname: "fab fa-java"
+    },
+    
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    
+  ]
+};
+
+
+// Your top 3 work experiences
+
+const workExperiences = {
+  viewExperiences: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Machine Learning Engineer",  
+      company: "BSNL",
+      companylogo: require("./assets/images/BSNL.png"),
+      date: "Aug 2016 – Oct 2016",
+      desc: "",
+      descBullets: [
+        "Engineered an algorithm that adaptively recommends next subscription plan by analyzing customers history of data usage",
+"Modeled and programmed a nonlinear regression model using R language and PostgreSQL database",
+"Learned about digital switching systems, digital transmission systems, telecommunication support infrastructure, and optical-fiber communication"
+      ]
+    },
+    {
+      role: "Software Engineer",  
+      company: "Amdocs",
+      companylogo: require("./assets/images/amdocs.jpeg"),
+      date: "June 2017 – Dec 2018",
+      desc: "",
+      descBullets: [
+        "Implemented code for converting design wireframes into website elements by using React, jQuery,HTML and JavaScript.",
+        
+        "Enhanced the scalability of front-end elements by optimizing them for loading speed and performance resulting in a 35x decrease in loading speed.",
+        
+        "Reduced manual testing efforts by developing REST API Automation Framework using Java’s Rest-Assured combined with TestNG",
+        
+        "Supervised junior developers in creating responsive modules using advanced CSS and JavaScript"
+      ]
+    },
+    {
+      role: "Software Engineer 2",   
+      company: "Amdocs",
+      companylogo: require("./assets/images/amdocs.jpeg"),
+      date: "Dec 2018 – Jul 2019",
+      desc: "",
+      descBullets: [
+        "Developed Back-end Web Services using AJAX, JSON and Java Spring for MySQL to make asynchronous calls to the project server to fetch data",
+
+"Automated functional testing modules using Selenium and Web Drivers that reduced testing efforts by 20x",
+
+"Mentored interns on projects spanning across automation framework, analyzing user data and generating statistical reports",
+
+"Developed Shell scripts to automate simple tasks for Linux environments involved in testing database applications in Oracle and SQL"
+
+      ]
+    },
+    {
+      role: "Graduate Research Assistant",  
+      company: "Electronic Visualization lab",
+      companylogo: require("./assets/images/UIC.png"),
+      date: "Dec 2019 – Aug 2020",
+      desc: "",
+      descBullets:[
+        "Assisting Gait Therapists to promote Locomotor Recovery for patients post-stroke by simulating a Virtual Reality environment on a treadmill.",
+"Using Unity to develop a virtual environment of Chicago Riverwalk with auto-generated obstacles of various difficulty levels.",
+"Creating an application using Node.js backend and React JS frontend to facilitate control over the virtual environment setup."
+      ]
+    },
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: "asaraf4", // Change to your github username to view your profile in Contact Section.
+  showGithubProfile :"true" // Set true or false to show Contact profile using Github, defaults to false 
+};
+
+
+// Some Big Projects You have worked with your company
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "The projects I'm really proud of",
+  projects: [
+    {
+  
+      image: require("./assets/images/Can you Escape.png"),
+      link: "https://sites.google.com/view/cs-428-can-you-escape/home?authuser=0",
+      
+    },
+   
+    
+    {
+
+      image: require("./assets/images/Dont judge.png"),
+      link: "https://marcobeccarini.wixsite.com/mysite"
+ 
+    },
+    
+    {
+      image: require("./assets/images/Dream cubicle.png"),
+      link: "https://sites.google.com/view/cptasz2cs428/projects/project-2?authuser=0"
+    },
+    {
+      image: require("./assets/images/Traffic Sign.png"),
+      link: "https://docs.google.com/document/d/110JaIa7-nnGNwynmfcOocH7NB7k-Db24CaxTL81xBIU/edit#heading=h.rv6hsfad8zu2"
+    },
+    {
+  
+      image: require("./assets/images/gun shot.png"),
+      link: "https://sites.google.com/view/cs-428-can-you-escape/home?authuser=0",
+      
+    }
+  ]
+};
+
+// Your Achievement Section Include Your Certification Talks and More
+
+const achievementSection = {
+
+  title: emoji("Achievements 🥇"),
+  subtitle: "Achievements, Conferences and some Hackathons that I have attended !",
+
+  achivementsCards: [
+    {
+      title: "Facebook Hackathon 2019",
+      subtitle: "Developed a game that could be played with just head movements",
+      image: require("./assets/images/Facebook.png"),
+      footerLink: [
+        { name: "Link to the project", url: "https://devpost.com/software/can-you-hop-zr8y7d" },
+        
+      ]
+    },
+    {
+      title: "Grace Hopper Conference 2020",
+      subtitle: "Attended as a Scholar from UIC",
+      image: require("./assets/images/GHC2020.png"),
+      footerLink: []
+    },
+
+  
+  ]
+};
+
+// Blogs Section
+
+const blogSection = {
+
+  title: "Extracurriculars",
+  subtitle: "Apart from my love for Computer Science, I love to sing and sketch",
+
+  blogs: [
+    {
+      url: "https://sites.google.com/view/aasthasaraf/gallery",
+      title: "Have a look at my Art Gallery",
+      description: "Art enables us to find ourselves and lose ourselves at the same time (Thomos Merton)"
+    },
+    {
+      url: "https://www.youtube.com/channel/UCDatR0bVd6zl9vKCLW9cakQ?view_as=subscriber",
+      title: "Some of my singing performances ",
+      description: "A song can be more than words and music ... when sung with soul a song carries you to another world, to a place where no matter how much pain you feel, you are never alone 🧘🏻‍♀"
+    },
+    {
+      url: "https://www.instagram.com/gtcurries/?hl=en",
+      title: "Need cooking tips anyone?",
+      description: "You ain't a foodie if hastag # doesn't look like a waffle to you? 😋"
+    }
+    
+  ]
+};
+
+// Talks Sections
+
+// const talkSection = {
+//   title: "",
+//   subtitle: emoji(""),
+
+//   talks: [
+//     {
+//       title: "",
+//       subtitle: "",
+//       slides_url: "",
+//       event_url: ""
+//     }
+//   ]
+// };
+
+// Podcast Section
+
+// const podcastSection = {
+//   title: emoji(""),
+//   subtitle: "",
+
+//   // Please Provide with Your Podcast embeded Link
+// podcast: [""]
+// };
+
+const contactInfo = {
+  title: emoji("Contact Me 📞"),
+  subtitle: "Feel free to get in touch 😄",
+  number: "+1-7737662666",
+  email_address: "aasthasaraf13@gmail.com"
+};
+
+//Twitter Section
+
+const twitterDetails = {
+
+  userName : ""//Replace "twitter" with your twitter username without @
+  
+};
+export { greeting, socialMediaLinks, skillsSection, techStack, workExperiences, openSource, bigProjects, achievementSection, blogSection, contactInfo,twitterDetails  };
