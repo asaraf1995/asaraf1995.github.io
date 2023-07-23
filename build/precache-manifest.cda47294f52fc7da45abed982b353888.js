49924643c2253ffe2c026d5ae3a745a8517d07d4
Aastha Saraf <aasthasaraf1995@gmail.com>
2023-07-22 22:42:44 -0700

    Build for portfolio 2.0

---
+++ build/precache-manifest.cda47294f52fc7da45abed982b353888.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f4ca0563a0374969b2f1bc050b460a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd4be2089608d008190e",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "4ea546fe7b26d9625c09",
+    "url": "/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "e15e188fde6daab1e7bd",
+    "url": "/static/css/main.6b13a1fd.chunk.css"
+  },
+  {
+    "revision": "126a4f8bb1f9434b564a",
+    "url": "/static/js/2.ca05fba2.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.ca05fba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4be2089608d008190e",
+    "url": "/static/js/3.aa622917.chunk.js"
+  },
+  {
+    "revision": "4ea546fe7b26d9625c09",
+    "url": "/static/js/4.55c318cd.chunk.js"
+  },
+  {
+    "revision": "e15e188fde6daab1e7bd",
+    "url": "/static/js/main.487a584c.chunk.js"
+  },
+  {
+    "revision": "e24c8f629b1733d127e7",
+    "url": "/static/js/runtime-main.38959816.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "d7ce2f9df5c5c809500a1aa5ba79b351",
+    "url": "/static/media/GHCLogo.d7ce2f9d.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "647e820a0eb94cea738c38b8f7170812",
+    "url": "/static/media/amazonMusic.647e820a.png"
+  },
+  {
+    "revision": "44b790166723c6ee5ec05973ebee0ba5",
+    "url": "/static/media/canYouEscape.44b79016.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "e74805fc34fd31a093bde2573770b4ed",
+    "url": "/static/media/covidProjectLogo.e74805fc.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "454cbed1e26d69dfc4f02d8b1bd4976a",
+    "url": "/static/media/gunshotProjectLogo.454cbed1.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "2e2ad9d9fc7eceaba7448d5396184a16",
+    "url": "/static/media/uicLogo.2e2ad9d9.png"
+  },
+  {
+    "revision": "54aaec0a8484538526dab0d5ed40e002",
+    "url": "/static/media/vrLogo.54aaec0a.png"
+  }
+]);
